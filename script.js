0b27bfa5d373b6e2112aab83a370044e2356975c
Sudhanshu Jain <motabhai0708@gmail.com>
2023-07-19 22:29:07 +0530

    function

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ let button=document.querySelectorAll('button')
 let string=" ";
 let arr=Array.from(button);
 arr.ForEach(button => {
-    button.addEventListener('click', )
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML =='='){
+            string= eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML =='AC'){
+            string="";
+            input.value=string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    } )
 })
