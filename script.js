5d7e393bc9f14c2cd3f294f0a6b791d47ce3cae1
Sudhanshu Jain <motabhai0708@gmail.com>
2023-07-19 16:28:41 +0530

    styling

---
+++ script.js
@@ -0,0 +1,8 @@
+let input=document.getElementById('inputbox');
+let button=document.querySelectorAll('button')
+
+let string=" ";
+let arr=Array.from(button);
+arr.ForEach(button => {
+    button.addEventListener('click', )
+})
